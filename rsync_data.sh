42f3c35dfeb4b05704b568dc5ac1658fba800f00
evanrex <evangeorgerex@gmail.com>
2024-03-12 21:22:31 +0000

    wip

--- rsync_data.sh
+++ rsync_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define source and destination directories
-SOURCE_DIR="/home/er647/projects/feature-wise-active-learning/data"
-DEST_DIR="/rds/user/er647/hpc-work/FWAL/data"
+SOURCE_DIR="/home/er647/projects/feature-wise-active-learning/data/"
+DEST_DIR="/rds/user/er647/hpc-work/FWAL/data/"
 
 # Perform rsync from source to destination
 rsync -avz $SOURCE_DIR $DEST_DIR
