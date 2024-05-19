0c7d26d4c90dcedc491932b3c0bfb7d897afaff0
evanrex <evangeorgerex@gmail.com>
2024-05-19 21:45:04 +0100

    experiments

---
+++ slurm_scripts/secondary_analysis_finance/mlp_9c64813a.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J mlp_9c64813a_finance
+#SBATCH --output=logs/out_%A.out
+#SBATCH --error=logs/err_%A.err
+#SBATCH -A COMPUTERLAB-SL2-CPU
+#SBATCH --time=10:00:00
+#SBATCH -p icelake
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
+module purge                               # Removes all modules still loaded
+module load rhel8/default-amp              # REQUIRED - loads the basic environment
+
+wandb agent evangeorgerex/fwal/ayygv27o
