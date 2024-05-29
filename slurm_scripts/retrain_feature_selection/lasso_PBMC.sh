e913d8bba46b23a28ee0bbc72aabbc7d2064bd88
evanrex <evangeorgerex@gmail.com>
2024-05-29 20:30:41 +0100

    experiment scripts (retrain feature select)

--- slurm_scripts/retrain_feature_selection/lasso_PBMC.sh
+++ slurm_scripts/retrain_feature_selection/lasso_PBMC.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#SBATCH -J fact_PBMC
+#SBATCH -J lasso_PBMC
 #SBATCH --output=logs/out_%A.out
 #SBATCH --error=logs/err_%A.err
 #SBATCH -A COMPUTERLAB-SL2-CPU
-#SBATCH --time=1:00:00
-#SBATCH -p icelake
+#SBATCH --time=10:00:00
+#SBATCH -p icelake-himem
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
+        #SBATCH --mem=13000
 
 . /etc/profile.d/modules.sh                # Leave this line (enables the module command)
 module purge                               # Removes all modules still loaded
 module load rhel8/default-amp              # REQUIRED - loads the basic environment
 
-wandb agent evangeorgerex/fwal/bx9ez4ps
+wandb agent evangeorgerex/fwal/5ww79p3y
